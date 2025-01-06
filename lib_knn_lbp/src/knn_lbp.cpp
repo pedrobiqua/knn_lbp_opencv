9a76c4eccaf6734c4eeec1559df5dcc3251f9191
Pedro Bianchini de Quadros <pedrobiqua@gmail.com>
2025-01-06 11:17:20 -0300

    feat(lib-knn_lbp): create project with cmake

---
+++ lib_knn_lbp/src/knn_lbp.cpp
@@ -0,0 +1,6 @@
+#include "knn_lbp.hpp"
+
+void say_hello()
+{
+    std::cout << "Hello, from knn_lbp!\n";
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+// Aqui vou rodar a lib
+#include "knn_lbp.hpp"
+
+int main(int argc, char const *argv[])
+{
+    say_hello();
+    return 0;
+}
